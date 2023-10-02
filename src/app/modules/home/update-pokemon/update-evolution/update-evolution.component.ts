2760bdc6660e4824140efe6358edc14d7dfdb489
Luis Ignacio Zamora Villagra <l>
2023-10-02 18:26:06 -0300

    Se elimina spec css y la referencia del css

--- src/app/modules/home/update-pokemon/update-evolution/update-evolution.component.ts
+++ src/app/modules/home/update-pokemon/update-evolution/update-evolution.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update-evolution',
-  templateUrl: './update-evolution.component.html',
-  styleUrls: ['./update-evolution.component.css']
+  templateUrl: './update-evolution.component.html'
 })
 export class UpdateEvolutionComponent implements OnInit {
 
